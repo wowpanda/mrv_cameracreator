d5aae19f0aa7b0f6251f4c0e8637643d63ec8ed7
MRVFivem <49911917+MRVFivem@users.noreply.github.com>
2019-08-15 18:21:01 +0200

    Update yourcamera.lua

--- mrv_cameracreator/yourcamera.lua
+++ mrv_cameracreator/yourcamera.lua
@@ -1,7 +1,1 @@
-CreateCamera = function()
-      if not DoesCamExist(cam) then
-      cam = CreateCamWithParams('DEFAULT_SCRIPTED_CAMERA', 128.129, 120.777, 90.12, -10.0, 0, 0, 60.0, true, 2)
-      SetCamActive(cam, true)
-      SetCamRot(cam2, 0.0, 0.0, 128.120, true) 
-   end
-end
+
